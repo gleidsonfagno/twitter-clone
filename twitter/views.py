ca35610ee2a40263aafab0dd741a638bafb774b6
gledsonfagno <gleidsonfagner838@gmail.com>
2024-07-13 18:59:21 -0300

    :sparkles: feat: editar Perfil de cada usuario

--- twitter/views.py
+++ twitter/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Profile, Post
-from .forms import UserRegisterForm, PostForm
+from .forms import UserRegisterForm, PostForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.models import User
 
 def home(request):
@@ -42,5 +42,19 @@ def profile(request, username):
     context = {'user':user, 'posts':posts}
     return render(request, 'twitter/profile.html', context)
 
+
 def editar(request):
-    return render(request, 'twitter/editar.html')
+	if request.method == 'POST':
+		u_form = UserUpdateForm(request.POST, instance=request.user)
+		p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+
+		if u_form.is_valid() and p_form.is_valid():
+			u_form.save()
+			p_form.save()
+			return redirect('home')
+	else:
+		u_form = UserUpdateForm(instance=request.user)
+		p_form = ProfileUpdateForm()
+
+	context = {'u_form' : u_form, 'p_form' : p_form}
+	return render(request, 'twitter/editar.html', context)
