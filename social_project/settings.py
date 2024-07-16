9d2549141e6a857f3079707409896dff3748505d
gledsonfagno <gleidsonfagner838@gmail.com>
2024-07-16 01:29:07 -0300

    atua lizando o static

--- social_project/settings.py
+++ social_project/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 
