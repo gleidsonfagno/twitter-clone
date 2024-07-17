a4c93bc68b0545a60e50b10a474a911f75ddd289
gledsonfagno <gleidsonfagner838@gmail.com>
2024-07-16 23:12:31 -0300

    Update settings.py to fix CSRF and static files issues, and add icon with text

--- social_project/settings.py
+++ social_project/settings.py
@@ -124,10 +124,15 @@ USE_TZ = True
 # STATIC_URL = '/static/'
 #  STATIC_ROOT = BASE_DIR / '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+CSRF_TRUSTED_ORIGINS = ['https://twitter-clone-production-b25d.up.railway.app']
+
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles'),
+]
 
 
 LOGIN_REDIRECT_URL = 'home'
