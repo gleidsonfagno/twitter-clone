3710954da6176f5da6df281762848f1a1a530da0
gledsonfagno <gleidsonfagner838@gmail.com>
2024-07-18 20:23:56 -0300

    tentando resolver os erros Not Found: da pasta static

--- social_project/settings.py
+++ social_project/settings.py
@@ -137,7 +137,7 @@ CSRF_TRUSTED_ORIGINS = ['https://twitter-clone-production-b25d.up.railway.app']
 # ]
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Diretório para arquivos estáticos coletados
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Diretório para arquivos estáticos coletados
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'twitter/static'),  # Diretório para arquivos estáticos específicos do app
